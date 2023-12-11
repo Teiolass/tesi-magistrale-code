bug fixing (off by one...)

kelso/explainer.py
@@ -5,6 +5,7 @@ import lib.generator as gen
 
 ontology_path  = 'data/processed/ontology.parquet'
 diagnoses_path = 'data/processed/diagnoses.parquet'
+ccs_path       = 'data/processed/ccs.parquet'
 
 def print_patient(ids: np.ndarray, cnt: np.ndarray, ontology: pl.DataFrame):
     codes = pl.DataFrame({'icd9_id':ids})
@@ -27,24 +28,28 @@ def print_patient(ids: np.ndarray, cnt: np.ndarray, ontology: pl.DataFrame):
 
 ontology  = pl.read_parquet(ontology_path)
 diagnoses = pl.read_parquet(diagnoses_path)
+ccs_data  = pl.read_parquet(ccs_path)
 
 diagnoses = diagnoses.head(100_000)
 
 unique_codes = diagnoses['icd9_id'].explode().unique().to_numpy()
+max_ccs_id = ccs_data['ccs_id'].max()
 
-codes  = list(diagnoses['icd9_id'].to_numpy())
-counts = list(diagnoses['count'  ].to_numpy())
+
+ccs_codes = list(diagnoses['ccs_id'].to_numpy())
+icd_codes = list(diagnoses['icd9_id'].to_numpy())
+counts    = list(diagnoses['count'  ].to_numpy())
 
 ontology_array = ontology[['icd9_id', 'parent_id']].to_numpy()
 gen.create_c2c_table(ontology_array, unique_codes)
 
 first = 100
 reference = 0
-result = gen.find_neighbours(codes[reference], counts[reference], codes[first:], counts[first:], 2);
+result = gen.find_neighbours(icd_codes[reference], counts[reference], icd_codes[first:], counts[first:], 2);
 best = np.argmin(result)
 
-# print('='*5 + '  PATIENT 1  ' + '='*5)
-# print_patient(codes[reference], counts[reference], ontology)
-# print('='*5 + '  PATIENT 2  ' + '='*5)
-# print_patient(codes[best+first], counts[best+first], ontology)
+print('='*5 + '  PATIENT 1  ' + '='*5)
+print_patient(icd_codes[reference], counts[reference], ontology)
+print('='*5 + '  PATIENT 2  ' + '='*5)
+print_patient(icd_codes[best+first], counts[best+first], ontology)
 